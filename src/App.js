65ccbbc20628d6a748fa4bebd00f1ebd089043c3
mathiastj <2278040+mathiastj@users.noreply.github.com>
2020-04-08 23:32:57 +0200

    Change data provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
+import buildHasuraProvider from 'ra-data-hasura-graphql';
 import { Admin, Resource, EditGuesser, ListGuesser, ShowGuesser } from 'react-admin';
 import {RecipeList, RecipeEdit, RecipeCreate, RecipeShow } from './components/recipes'
-import hasuraDataProvider from 'ra-data-hasura';
 import authProvider from './authProvider'
+import ApolloClient from 'apollo-boost'
 
-const key = localStorage.getItem('key')
-const headers = {'content-type': 'application/json', 'x-hasura-admin-secret': key};
 
-const App = () => <Admin
-  dataProvider={hasuraDataProvider('https://floating-meadow-53258.herokuapp.com', headers)}
-  authProvider={authProvider}
-  >
-  <Resource name="recipes" show={RecipeShow} list={RecipeList} edit={RecipeEdit} create={RecipeCreate}/>
-</Admin>
+
+
+class App extends Component {
+  constructor() {
+      super();
+      this.state = { dataProvider: null };
+  }
+  async componentDidMount() {
+    const key = localStorage.getItem('key')
+    const headers = {'content-type': 'application/json', 'x-hasura-admin-secret': key}; 
+    const client = new ApolloClient({uri: 'https://floating-meadow-53258.herokuapp.com/v1/graphql', headers: headers});
+
+    const dataProvider = await buildHasuraProvider({ client: client })
+    this.setState({ dataProvider })
+  }
+
+  render() {
+      const { dataProvider } = this.state;
+
+      if (!dataProvider) {
+          return <div>Loading</div>;
+      }
+
+      return (
+        <Admin
+        dataProvider={dataProvider}
+        authProvider={authProvider}
+        >
+        <Resource name="recipes" show={RecipeShow} list={RecipeList} edit={RecipeEdit} create={RecipeCreate}/>
+      </Admin>
+      );
+  }
+}
 
 export default App;
