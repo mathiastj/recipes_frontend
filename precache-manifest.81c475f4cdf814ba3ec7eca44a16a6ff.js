72a39e0efe5cae7c91c2811e3e19cf4515257ec6
mathiastj <2278040+mathiastj@users.noreply.github.com>
2020-04-11 16:01:55 +0200

    Updates

---
+++ precache-manifest.81c475f4cdf814ba3ec7eca44a16a6ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659819032a18ce2e816f3375fd2867f5",
+    "url": "/recipes-frontend/index.html"
+  },
+  {
+    "revision": "082481cbee2a0b5a3d63",
+    "url": "/recipes-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9edf685534962ab01847",
+    "url": "/recipes-frontend/static/js/2.7c1dc85c.chunk.js"
+  },
+  {
+    "revision": "b9f84bc3c1104ff90527428a61dd5ec9",
+    "url": "/recipes-frontend/static/js/2.7c1dc85c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082481cbee2a0b5a3d63",
+    "url": "/recipes-frontend/static/js/main.7ed7c4ac.chunk.js"
+  },
+  {
+    "revision": "f0e1fdb610c01beaf442",
+    "url": "/recipes-frontend/static/js/runtime-main.bb25dcd5.js"
+  }
+]);
